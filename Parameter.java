3284447455038cb474ae00e6989147fba0f7287b
apaland <apaland@live.de>
2024-11-16 10:54:07 +0100

    Add files via upload

---
+++ Parameter.java
@@ -0,0 +1,15 @@
+
+public class Parameter {
+  static String ApiKey;
+  static String ApiUrl; 
+  static String User;
+  static String GDTExportPath;
+  static String GDTArchivePath;
+  static String DBFPath;
+  static String resmedTenant;
+  static String primaryDoctorId;
+  static String regionId;
+  static String classification;
+  static String ErweiterteKonsoleMeldung;
+}
+
